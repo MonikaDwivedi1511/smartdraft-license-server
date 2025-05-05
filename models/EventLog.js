48f5f9ff14e609759a21322d644965658c609859
MonikaDwivedi1511 <monikadwivedi15@gmail.com>
2025-05-06 02:19:02 +0530

    Update EventLog.js

--- models/EventLog.js
+++ models/EventLog.js
@@ -1,11 +1,15 @@
+
+
 const mongoose = require("mongoose");
 
 const EventLogSchema = new mongoose.Schema({
-  event: String,
-  licenseKey: String,
-  plan: String,
-  draftId: String,
+  event: { type: String, required: true },
+  licenseKey: { type: String, required: true },
+  plan: { type: String, default: "trial" },
+  draftId: { type: String, default: null },
+  metadata: { type: Object, default: {} },
   timestamp: { type: Date, default: Date.now }
 });
 
 module.exports = mongoose.model("EventLog", EventLogSchema);
+
