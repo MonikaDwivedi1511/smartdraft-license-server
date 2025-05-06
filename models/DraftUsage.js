390b561091891534f79900f24ec03bb3b38cd534
MonikaDwivedi1511 <monikadwivedi15@gmail.com>
2025-05-06 11:34:26 +0530

    Update DraftUsage.js

--- models/DraftUsage.js
+++ models/DraftUsage.js
@@ -1,10 +1,12 @@
+// models/DraftUsage.js
 const mongoose = require("mongoose");
 
 const DraftUsageSchema = new mongoose.Schema({
   licenseKey: String,
   plan: String,
   variant: String,
-  usedAt: { type: Date, default: Date.now }
+  usedCount: Number,
+  timestamp: { type: Date, default: Date.now }
 });
 
 module.exports = mongoose.model("DraftUsage", DraftUsageSchema);
