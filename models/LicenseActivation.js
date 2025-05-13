6fb24897e8f1770b71385b1045936f5e2552e06e
MonikaDwivedi1511 <monikadwivedi15@gmail.com>
2025-05-13 21:01:36 +0530

    Update LicenseActivation.js

--- models/LicenseActivation.js
+++ models/LicenseActivation.js
@@ -6,9 +6,11 @@ const LicenseActivationSchema = new mongoose.Schema({
   orderId: String,
   expiresAt: Date,
   activatedAt: { type: Date, default: Date.now },
-  clientId: { type: String, required: false}, // ✅ Add this line
+  clientId: { type: String, required: true}, // ✅ Add this line
   lastClientIdSwitchAt: { type: Date, default: null },
   switchCount: { type: Number, default: 0 },
+  status: { type: String, default: "active" },
+  activatedAt: Date
 });
 
 module.exports = mongoose.model("LicenseActivation", LicenseActivationSchema);
